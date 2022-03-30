024ef5373502d5d6cd42d1ead79749147d20095a
yasincelebi <yasincelebix@gmail.com>
2022-03-30 22:18:55 +0300

    create project with angular-cli

---
+++ projects/ngx-depend-dropdown/src/public-api.ts
@@ -0,0 +1,11 @@
+/*
+ * Public API Surface of ngx-depend-dropdown
+ */
+
+// <reference path="./lib/types/dropdown.d.ts" />
+export * from './lib/ngx-depend-dropdown.service';
+export * from './lib/ngx-depend-dropdown.component';
+export * from './lib/ngx-depend-dropdown.module';
+
+
+
