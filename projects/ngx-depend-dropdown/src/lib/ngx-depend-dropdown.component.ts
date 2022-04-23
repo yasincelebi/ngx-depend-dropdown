2616fe59094d813bc5ecf1419acfe8775f773004
yasincelebi <yasincelebix@gmail.com>
2022-04-23 22:28:21 +0300

    type bugfix

--- projects/ngx-depend-dropdown/src/lib/ngx-depend-dropdown.component.ts
+++ projects/ngx-depend-dropdown/src/lib/ngx-depend-dropdown.component.ts
@@ -1,105 +1,110 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChildren} from '@angular/core';
-import {NgxDependDropdownService} from "./ngx-depend-dropdown.service";
-import {Observable, of} from "rxjs";
-import {DropdownConfig, DropdownData, DropdownNode} from "../typings";
-
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChildren,
+} from '@angular/core';
+import { NgxDependDropdownService } from './ngx-depend-dropdown.service';
+import { Observable, of } from 'rxjs';
+import {
+  DropdownConfig,
+  DropdownData,
+  DropdownNode,
+} from '../types/ngx-depend-dropdown';
 
 @Component({
   selector: 'ngx-depend-dropdown',
   templateUrl: `ngx-depend-dropdown.component.html`,
-  styles: []
+  styles: [],
 })
 export class NgxDependDropdownComponent implements OnInit, AfterViewInit {
-  @Input() data: DropdownData = []
-  @Input() config: DropdownConfig = {}
-  @Output() onChange: EventEmitter<{ nativeEvent: Event, changedDropdown: DropdownNode, fullData: DropdownData }> = new EventEmitter()
-  @ViewChildren('dropdown') dropdown: any
+  @Input() data: DropdownData = [];
+  @Input() config: DropdownConfig = {};
+  @Output() onChange: EventEmitter<{
+    nativeEvent: Event;
+    changedDropdown: DropdownNode;
+    fullData: DropdownData;
+  }> = new EventEmitter();
+  @ViewChildren('dropdown') dropdown: any;
 
-  constructor(private dropdownService: NgxDependDropdownService) {
-
-
-  }
+  constructor(private dropdownService: NgxDependDropdownService) {}
 
   ngAfterViewInit() {
-    this.data.forEach((item, index: any) => {
+    this.data.forEach((item: DropdownNode, index: any) => {
       if (!item.visibilityValue && this.config.unavailableType === 'disabled') {
-        this.dropdown.toArray()[index].unavailableType.next('disabled')
+        this.dropdown.toArray()[index].unavailableType.next('disabled');
       }
-    })
-
+    });
   }
 
-
   ngOnInit(): any {
-
-
     this.dropdownService.data = this.data;
     this.dropdownService.dropdownConfig = this.config;
-    this.init()
+    this.init();
   }
 
   arrayToObservable(array: Observable<any> | any[]): Observable<any> {
     if (Array.isArray(array)) {
-      return of(array)
+      return of(array);
     }
-    return array
+    return array;
   }
 
-  select(args: { $event: Event, data: DropdownNode }): void {
+  select(args: { $event: Event; data: DropdownNode }): void {
     this.data.forEach((node: DropdownNode, index: any) => {
-
       if (args.data.name === node.dependent) {
-        this.setValues(node)
-        node.selectedValue = null
+        this.setValues(node);
+        node.selectedValue = null;
       }
 
-      const nodeValue = this.dropdownService.getNodeValue(node?.dependent)
+      const nodeValue = this.dropdownService.getNodeValue(node?.dependent);
 
       if (node.dependent && !nodeValue) {
-        node.selectedValue = null
+        node.selectedValue = null;
       }
 
+      node.visibilityValue = node.visibility() || false;
+      const isDisabled =
+        !node.visibilityValue &&
+        this.config.unavailableType === 'disabled' &&
+        node.dependent;
+      this.dropdown.toArray()[index].unavailableType.next(isDisabled);
+    });
 
-      node.visibilityValue = node.visibility() || false
-      const isDisabled = !node.visibilityValue && this.config.unavailableType === 'disabled' && node.dependent
-      this.dropdown.toArray()[index].unavailableType.next(isDisabled)
-
-    })
-
-    this.onChange.emit({nativeEvent: args.$event, changedDropdown: args.data, fullData: this.data})
+    this.onChange.emit({
+      nativeEvent: args.$event,
+      changedDropdown: args.data,
+      fullData: this.data,
+    });
   }
 
   init() {
-
     this.data.forEach((node: DropdownNode) => {
-
       if (!node.placeholder) {
-        node.placeholder = 'Select'
+        node.placeholder = 'Select';
       }
 
       node.visibilityValue = node.visibility();
-      node.selectedValue = node.selectedValue || null
-     this.setValues(node)
-
+      node.selectedValue = node.selectedValue || null;
+      this.setValues(node);
     });
-
-
   }
 
-  setValues(node: DropdownNode){
+  setValues(node: DropdownNode) {
     let val = node.setValues();
 
-    let observableArray = this.arrayToObservable(val)
+    let observableArray = this.arrayToObservable(val);
 
     observableArray.subscribe((values: any) => {
       values.find((item: any) => {
         if (item.id === node.selectedValue) {
-          node.selectedValue = item
+          node.selectedValue = item;
         }
-      })
+      });
       node.values = values;
-    })
+    });
   }
-
-
 }
--- projects/ngx-depend-dropdown/src/public-api.ts
+++ projects/ngx-depend-dropdown/src/public-api.ts
@@ -2,10 +2,7 @@
  * Public API Surface of ngx-depend-dropdown
  */
 
-// <reference path="./lib/types/dropdown.d.ts" />
+// <reference path="./typings.d.ts" />
 export * from './lib/ngx-depend-dropdown.service';
 export * from './lib/ngx-depend-dropdown.component';
 export * from './lib/ngx-depend-dropdown.module';
-
-
-
